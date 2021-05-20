f3818a3790e03b943dd444381af108c4d0b5e484
Fidel <fidel.tonak@stud.hs-hannover.de>
2021-05-20 18:51:38 +0200

    Funktioniert eigentlich alles aber noch automatische Klickanzahl ermitteln (Mittels scrollheight)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
 import time
 from selenium import webdriver
+from random import randint
 
 pcBenutzername = "R11111111111"
 options = webdriver.ChromeOptions()
@@ -7,6 +9,7 @@ options.add_argument("user-data-dir=C:/Users/" + pcBenutzername + "/AppData/Loca
 browser = webdriver.Chrome(executable_path="D:/chromedriver_win32/chromedriver.exe", options=options)
 followerNamen = []
 kommentarNamen = []
+anzahlKommentareLaden = input("Wie oft Kommentare laden? ")
 
 instaSeite = 'https://www.instagram.com/ink.busters/'
 instaFollowSeite = 'https://www.instagram.com/ink.busters/followers/'
@@ -54,29 +57,67 @@ while True:
 
 
 def restzeug():
-    print("Followerarraylänge: " + str(len(followerNamen)))
-    print("Liker abholen...")
+    print(str(len(followerNamen)))
+    for x in range(100):
+        print("I N K B U S T E R S - - - G E W I N N S P I E L")
+    print("Storyposter abholen...")
+    print("Kommentare abholen...")
     browser.get(instaBildSeite)
-    kommentarleute = []
-    print("Kommentararray erstellt!")
-    kommentareladenknopf = browser.find_element_by_css_selector('.dCJp8.afkep')
-    for x in range(15):
-        kommentareladenknopf.click()
-        time.sleep(1)
-    print(browser.find_element_by_xpath(
-        "tml/body/div[1]/section/main/div/div[1]/article/div[3]/div[1]/ul/ul[1]/div/li/div/div[1]/div[2]/span"))
-    for x in range(1, 100):
-        kommentarleute[x - 1] = browser.find_element_by_xpath(
-            "tml/body/div[1]/section/main/div/div[1]/article/div[3]/div[1]/ul/ul[" + str(
-                x) + "]/div/li/div/div[1]/div[2]/span")
+    time.sleep(5)
+    try:
+        load_more_comment = browser.find_element_by_css_selector('.NUiEW > button:nth-child(1)')
+        # print("Found {}".format(str(load_more_comment)))
+        i = 0
+        while load_more_comment.is_displayed() and i < int(anzahlKommentareLaden):
+            load_more_comment.click()
+            time.sleep(1.5)
+            load_more_comment = browser.find_element_by_css_selector('.NUiEW > button:nth-child(1)')
+            # print("Found {}".format(str(load_more_comment)))
+            i += 1
+    except Exception as e:
+        print(e)
+        pass
+
+    user_names = []
+    user_comments = []
+    comment = browser.find_elements_by_class_name('gElp9 ')
+    for c in comment:
+        container = c.find_element_by_class_name('C4VMK')
+        name = container.find_element_by_class_name('_6lAjh').text
+        content = container.find_element_by_tag_name('span').text
+        content = content.replace('\n', ' ').strip().rstrip()
+        user_names.append(name)
+        user_comments.append(content)
+    user_names.pop(0)
+    user_comments.pop(0)
+    user_names = list(dict.fromkeys(user_names))
+    gewinnspielliste = []
+    followerkurz = []
+    for follower in followerNamen:
+        followerkurz.append(follower.replace('https://www.instagram.com/', '').replace('/', ''))
+    for username in user_names:
+        if username in followerkurz:
+            gewinnspielliste.append(username)
+    # print(user_names)
+    gewinner = random.choice(gewinnspielliste)
     print("I N K B U S T E R S - - - G E W I N N S P I E L")
     go = False
     while not go:
         goprompt = input("go um zu starten ")
         if goprompt == 'go':
             go = True
-    for follower in followerNamen:
-        print(follower.replace('https://www.instagram.com/', '').replace('/', ''))
+    print("Gewinner ermitteln...")
+    time.sleep(randint(5, 10))
+    print(gewinner)
+    while True:
+        nochmalprompt = input("nochmal? ja / nein ")
+        if nochmalprompt == 'ja':
+            gewinner = random.choice(gewinnspielliste)
+            print("Gewinner ermitteln...")
+            time.sleep(randint(5, 10))
+            print(gewinner)
+        else:
+            exit(0)
 
 
 try:
